e26e6cf1ae43eb1afc316018650b72e835e675a2
NatNat201 <62999186+NatNat201@users.noreply.github.com>
2020-04-21 16:45:07 +0200

    Encore accueil

--- Projet cooking/MainWindow.xaml.cs
+++ Projet cooking/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Projet_cooking
 {
@@ -27,5 +30,42 @@ namespace Projet_cooking
             windowClient w = new windowClient();
             w.Show();
         }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "SERVER=localhost;PORT=3306;DATABASE=cooking;UID=root;PASSWORD=SQL.ESILV.Comete.99;Convert Zero Datetime=True";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            try
+            {
+                string query = "select count(1) from client where mailClient=@mailClient and mdpClient=@mdpClient;";
+                MySqlCommand sqlCmd = new MySqlCommand(query, connection);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@mailClient", txtMail.Text);
+                sqlCmd.Parameters.AddWithValue("@mdpClient", txtMDP.Text);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Le mail et le mot de passe ne correspondent pas.");
+                };
+            }
+            catch(Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+
+        }
+
     }
 }
